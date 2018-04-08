de73fe66c0ddc4e897668b7d3ca869668d2d7fdc
Patrick Arminio <patrick.arminio@gmail.com>
2018-04-08 23:58:27 +0100

    Test errors

--- backend/tests/api/users/test_register.py
+++ backend/tests/api/users/test_register.py
@@ -54,3 +54,109 @@ def test_registration_works(graphql_client):
     uid = session.get_decoded().get('_auth_user_id')
 
     assert uid == str(user.id)
+
+
+@pytest.mark.django_db
+def test_registration_with_errors(graphql_client):
+    resp = graphql_client.query('''
+        mutation Register($input: RegistrationInput!) {
+            register(input: $input) {
+                ok
+                user {
+                    fullName
+                }
+                errors {
+                    fullName
+                    teamName
+                    email
+                    password
+                    nonFieldErrors
+                }
+            }
+        }
+    ''', variables={
+        'input': {
+            'fullName': 'Patrick Arminio',
+            'teamName': 'ORO 🏆',
+            'email': '',
+            'password': 'do you even 🏋🏼‍',
+        }
+    })
+
+    assert 'errors' not in resp
+
+    data = resp['data']['register']
+
+    assert data['errors']['email'] == ['This field is required.']
+
+
+@pytest.mark.django_db
+def test_registration_bad_email(graphql_client):
+    resp = graphql_client.query('''
+        mutation Register($input: RegistrationInput!) {
+            register(input: $input) {
+                ok
+                user {
+                    fullName
+                }
+                errors {
+                    fullName
+                    teamName
+                    email
+                    password
+                    nonFieldErrors
+                }
+            }
+        }
+    ''', variables={
+        'input': {
+            'fullName': 'Patrick Arminio',
+            'teamName': 'ORO 🏆',
+            'email': 'aaaaa',
+            'password': 'do you even 🏋🏼‍',
+        }
+    })
+
+    assert 'errors' not in resp
+
+    data = resp['data']['register']
+
+    assert data['errors']['email'] == ['Enter a valid email address.']
+
+
+@pytest.mark.django_db
+def test_registration_unique_email(graphql_client):
+    pat = User.objects.create(email='patrick.arminio@gmail.com')
+
+    resp = graphql_client.query('''
+        mutation Register($input: RegistrationInput!) {
+            register(input: $input) {
+                ok
+                user {
+                    fullName
+                }
+                errors {
+                    fullName
+                    teamName
+                    email
+                    password
+                    nonFieldErrors
+                }
+            }
+        }
+    ''', variables={
+        'input': {
+            'fullName': 'Patrick Arminio',
+            'teamName': 'ORO 🏆',
+            'email': pat.email,
+            'password': 'do you even 🏋🏼‍',
+        }
+    })
+
+    assert 'errors' not in resp
+
+    data = resp['data']['register']
+
+    assert data['errors']['email'] == [
+        'User with this Email address already exists.'
+    ]
