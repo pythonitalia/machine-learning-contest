6db2946329a2c64adc00ea8d93203a2ca4a49a41
Patrick Arminio <patrick.arminio@gmail.com>
2018-04-10 20:09:39 +0100

    Test that returns challenge data

--- backend/tests/api/challenges/test_query_challenges.py
+++ backend/tests/api/challenges/test_query_challenges.py
@@ -1,5 +1,7 @@
 import pytest
 
+from challenges.models import Challenge
+
 
 @pytest.mark.django_db
 def test_returns_empty_when_no_challenges(graphql_client):
@@ -14,3 +16,21 @@ def test_returns_empty_when_no_challenges(graphql_client):
     assert 'errors' not in resp
 
     assert resp['data']['challenges'] == []
+
+
+@pytest.mark.django_db
+def test_returns_challenges(graphql_client):
+    Challenge.objects.create(name='demo')
+
+    resp = graphql_client.query('''
+        query {
+            challenges {
+                name
+            }
+        }
+    ''')
+
+    assert 'errors' not in resp
+
+    assert len(resp['data']['challenges']) == 1
+    assert resp['data']['challenges'][0]['name'] == 'demo'
