b5f7ddaba7afb6bcbe9377a5e52ea962dfcb7a8f
Patrick Arminio <patrick.arminio@gmail.com>
2018-04-20 13:02:47 +0200

    Show leaderboard on the frontend

--- frontend/src/pages/leaderboard/index.js
+++ frontend/src/pages/leaderboard/index.js
@@ -1,34 +1,49 @@
 import React, { PureComponent } from "react";
 import { withStyles } from "material-ui/styles";
 import Typography from "material-ui/Typography";
-import {
-  ListItem,
-  ListItemIcon,
-  ListItemText
-} from "material-ui/List";
+import { ListItem, ListItemIcon, ListItemText } from "material-ui/List";
 import GradeIcon from "material-ui-icons/Grade";
+
+import { gql } from "apollo-boost";
+import { Query } from "react-apollo";
 
 const styles = theme => ({});
 
-const winners = ["Valerio Maggio", "Ernesto Arbitrio"];
+const LEADERBOARD = gql`
+  query {
+    leaderboard {
+      name
+      score
+    }
+  }
+`;
 
 class Leaderboard extends PureComponent {
   render() {
-
     return (
       <div>
         <Typography variant="display1" style={{ marginBottom: 40 }}>
           Leaderboard
         </Typography>
 
-        {winners.map(x => (
-          <ListItem key={x}>
-            <ListItemIcon>
-              <GradeIcon />
-            </ListItemIcon>
-            <ListItemText primary={x} />
-          </ListItem>
-        ))}
+        <Query query={LEADERBOARD}>
+          {({ loading, error, data }) => {
+            if (loading) return <div>Loading...</div>;
+            if (error) return <div>Error :(</div>;
+            if (data.leaderboard.length === 0)
+              return <div>No submission yet</div>;
+
+            return data.leaderboard.map(x => (
+              <ListItem key={x.name}>
+                <ListItemIcon>
+                  <GradeIcon />
+                </ListItemIcon>
+                <ListItemText primary={x.name || "no name"} />
+                <ListItemText primary={x.score} />
+              </ListItem>
+            ));
+          }}
+        </Query>
       </div>
     );
   }
