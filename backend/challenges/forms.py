430066d0ec0115413a1aee8c90018cb71903565a
Patrick Arminio <patrick.arminio@gmail.com>
2018-04-20 08:29:13 +0200

    Return fake result

--- backend/challenges/forms.py
+++ backend/challenges/forms.py
@@ -11,6 +11,9 @@ class SubmissionForm(forms.ModelForm):
     def save(self, user):
         submission = super().save(commit=False)
         submission.submitted_by = user
+
+        submission.score = 123
+
         submission.save()
 
         return submission
